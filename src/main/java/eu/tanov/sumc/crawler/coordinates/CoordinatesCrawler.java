746c6d8ff118a485fe4854128023df1af3e6c220
Plamen Tanov <1.plamen@gmail.com>
2010-07-25 15:09:59 +0300

    Added checking for output files

--- src/main/java/eu/tanov/sumc/crawler/coordinates/CoordinatesCrawler.java
+++ src/main/java/eu/tanov/sumc/crawler/coordinates/CoordinatesCrawler.java
@@ -1,6 +1,16 @@
 package eu.tanov.sumc.crawler.coordinates;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.apache.log4j.Logger;
+
+import eu.tanov.sumc.crawler.configuration.ConfigurationCrawler;
+import eu.tanov.sumc.crawler.model.SumcConfiguration;
+
 public class CoordinatesCrawler implements Runnable {
+	private static final Logger log = Logger.getLogger(CoordinatesCrawler.class.getName());
 
 	private final String outputFilename;
 	private final String configurationFilename;
@@ -18,7 +28,51 @@ public class CoordinatesCrawler implements Runnable {
 
 	public void run() {
 		// TODO Auto-generated method stub
+//		check output file
+//		chech log file
+		//get old coordinates
+		//fill map
+
+		//get configuration
+		//for each in configuration .. get coordinates from old coordinates
+		//if not - add to list
+		
+		//iterate list
+		
+		//get list with new
+		//get list with removed
+		
+		if (!checkFiles()) {
+			return;
+		}
 		
 	}
 
+	/**
+	 * save time if file could not be opened - do not crawl whole site
+	 */
+	private boolean checkFiles() {
+		return checkFile(outputFilename) && checkFile(logFilename);
+	}
+
+	/**
+	 * XXX improve
+	 */
+	private boolean checkFile(String filename) {
+		try {
+			writeToFile(filename, "check write permissions");
+			return true;
+		} catch (IOException e) {
+			log.error("could not write to "+filename, e);
+			return false;
+		}
+	}
+
+	private void writeToFile(String filename, String content) throws IOException {
+		final FileWriter outFile = new FileWriter(filename);
+		final PrintWriter out = new PrintWriter(outFile);
+		out.println(content);
+		out.close();
+		outFile.close();
+	}
 }
