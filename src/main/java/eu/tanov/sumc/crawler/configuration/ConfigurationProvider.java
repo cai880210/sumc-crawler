1c00a92ab940d2f5cb7c532f008d857c5fd319b8
Plamen Tanov <1.plamen@gmail.com>
2010-07-25 12:44:14 +0300

    Removed commented code

--- src/main/java/eu/tanov/sumc/crawler/configuration/ConfigurationProvider.java
+++ src/main/java/eu/tanov/sumc/crawler/configuration/ConfigurationProvider.java
@@ -23,8 +23,7 @@ public class ConfigurationProvider {
 	private static final String NAME_COMBO_LINES = "ctl00$ContentPlaceHolder1$ddlLines";
 	private static final String NAME_RADIO_DIRECTION = "ctl00$ContentPlaceHolder1$rblRoute";
 	private static final String NAME_COMBO_BUS_STOPS = "ctl00$ContentPlaceHolder1$ddlStops";
-//	private static final String NAME_IMAGE_MAP = "ctl00$ContentPlaceHolder1$imgMap";
-	//FIXME use By.name() instead:
+	//XXX use By.name() instead:
 	private static final String FORMAT_XPATH_BY_NAME = "//*[@name='%s']";
 //	private static final String FORMAT_XPATH_BY_FOR = "//*[@for='%s']";
 //	private static final String FORMAT_XPATH_BY_ID = "//*[@id='%s']";
@@ -35,8 +34,6 @@ public class ConfigurationProvider {
 	 */
 	private static final String URL_MAIN = "http://gps.skgt-bg.com/";
 	private static final int DEFAULT_TIMEOUT = 10000;
-//	private static final String ATTRIBUTE_IMAGE_SRC = "src";
-//	private static final char SEPARATOR_URL_PARAMETER = '=';
 	private static final char BUS_STOP_CODE_PREFIX = '(';
 	private static final char BUS_STOP_CODE_SUFFIX = ')';
 	private static final String BUS_STOP_CODE_SUFFIX_REGEX = Pattern.quote(String.valueOf(BUS_STOP_CODE_SUFFIX));
@@ -79,22 +76,7 @@ public class ConfigurationProvider {
 		setVehicleType(vehicleType);
 		return getList(NAME_COMBO_LINES, true);
 	}
-	/**
-	 * for select boxes (they are always visible)
-	 */
-//	private void waitForAnswer(final WebElement oldElement, final String elementName) {
-//		WaitHelper.waitForCondition(new Condition() {
-//			public boolean completed() {
-//				//check if there is new element
-//				final WebElement matched = webDriver.findElement(By.xpath(String.format(FORMAT_XPATH_BY_NAME, elementName)));
-//				return !oldElement.equals(matched);
-//			}
-//		}, DEFAULT_TIMEOUT);
-//	}
-	/**
-	 * for radio buttons (they are hidden if line is not selected)
-	 * using findElements() because findElement() throws if nothing found
-	 */
+
 	private void waitForAnswer(final List<WebElement> oldElements, final String elementName) {
 		WaitHelper.waitForCondition(new Condition() {
 			public boolean completed() {
@@ -116,40 +98,11 @@ public class ConfigurationProvider {
 			}
 		}, DEFAULT_TIMEOUT);
 	}
-//
-//	private void waitForAnswer(final String isChangedConditionName) {
-//		WaitHelper.waitForCondition(new Condition() {
-//			public boolean completed() {
-//				try {
-//					final List<WebElement> condition = webDriver.findElements(By.xpath(String.format(FORMAT_XPATH_BY_NAME, isChangedConditionName)));
-//					
-//					if (condition.size() == 0) {
-//						//not found - wait...
-//						return false;
-//					}
-//					if (condition.size()>1) {
-//						//radio buttons... found
-//						return true;
-//					}
-//
-//					//one element - it is select
-//					
-//					return SelectWebElementHelper.getOptions(condition.get(0)).size()>1;
-//				} catch (StaleElementReferenceException e) {
-//					//content is just refreshing by JS
-//					return false;
-//				}
-//			}
-//		}, DEFAULT_TIMEOUT);
-//	}
 
 	private void setVehicleType(String vehicleType) {
 		final WebElement vehicleTypes = webDriver.findElement(By.xpath(String.format(FORMAT_XPATH_BY_NAME, NAME_COMBO_VEHICLE_TYPES)));
 		//is need to change?
 		final WebElement selectedVehicleType = SelectWebElementHelper.getSelectedOption(vehicleTypes);
-//		if (selectedVehicleType == null) {
-//			throw new IllegalStateException("no selected vehicle type?!");
-//		}
 		if (selectedVehicleType!=null && vehicleType.equals(WebElementHelper.getText(selectedVehicleType))) {
 			//no need to change
 			return;
@@ -168,9 +121,6 @@ public class ConfigurationProvider {
 		
 		//is need to change?
 		final WebElement selectedLine = SelectWebElementHelper.getSelectedOption(lines);
-//		if (selectedLine == null) {
-//			throw new IllegalStateException("no selected line?!");
-//		}
 		if (selectedLine!=null && line.equals(WebElementHelper.getText(selectedLine))) {
 			//no need to change
 			return;
@@ -206,19 +156,7 @@ public class ConfigurationProvider {
 	public List<BusStop> getBusStops(String vehicleType, String line, boolean firstDirection) {
 		setDirection(vehicleType, line, firstDirection);
 		log.debug("vehicleType: " +vehicleType + ", line: " + line + ", firstDirection: "+firstDirection);
-//		WaitHelper.waitForCondition(new Condition() {
-//			public boolean completed() {
-//				try {
-//					final WebElement stops = webDriver.findElement(By.xpath(String.format(FORMAT_XPATH_BY_NAME, NAME_COMBO_BUS_STOPS)));
-//					final List<WebElement> selectOptions = SelectWebElementHelper.getOptions(stops);
-//					return selectOptions.size()>1;
-//				} catch (StaleElementReferenceException e) {
-//					//content is just refreshing by JS
-//					return false;
-//				}
-//			}
-//		}, DEFAULT_TIMEOUT);
-		
+
 		return parseBusStopsNames(getList(NAME_COMBO_BUS_STOPS, true));
 	}
 	
@@ -229,34 +167,6 @@ public class ConfigurationProvider {
 		}
 		return result;
 	}
-
-//	private void setBusStop(String vehicleType, String line, boolean firstDirection, final String busStop) {
-//		setDirection(vehicleType, line, firstDirection);
-//
-//		final WebElement busStops = webDriver.findElement(By.xpath(String.format(FORMAT_XPATH_BY_NAME, NAME_COMBO_BUS_STOPS)));
-//		WebElementHelper.setValue(busStops, busStop);
-//
-//		//wait new results
-//		waitForAnswer(NAME_IMAGE_MAP);
-//	}
-
-//	public BusStop getBusStop(String vehicleType, String line, boolean firstDirection, String busStopName) {
-//		log.info("getting "+vehicleType+", "+line+", "+firstDirection+", "+busStopName);
-//
-//		setBusStop(vehicleType, line, firstDirection, busStopName);
-//
-//		final WebElement image = webDriver.findElement(By.xpath(String.format(FORMAT_XPATH_BY_NAME, NAME_IMAGE_MAP)));
-//		final String src = image.getAttribute(ATTRIBUTE_IMAGE_SRC);
-//		if (src == null || src.lastIndexOf(SEPARATOR_URL_PARAMETER) == -1) {
-//			throw new IllegalStateException("bad src for: "+vehicleType+", "+line+", "+firstDirection+", "+busStopName+": "+src);
-//		}
-//		//skip SEPARATOR_URL_PARAMETER char
-//		final String bgMapsId = src.substring(src.lastIndexOf(SEPARATOR_URL_PARAMETER)+1);
-//		final BusStop result = new BusStop();
-//		result.setBgmapsId(bgMapsId);
-//		parseBusName(result, busStopName);
-//		return result;
-//	}
 
 	private BusStop createBusStopName(String busStopName) {
 		final BusStop result = new BusStop();
@@ -270,8 +180,6 @@ public class ConfigurationProvider {
 		result.setName(split[1].trim());
 
 		result.setCode(split[0].substring(1));
-
-//		log.trace("adding : " +result);
 
 		return result;
 	}
