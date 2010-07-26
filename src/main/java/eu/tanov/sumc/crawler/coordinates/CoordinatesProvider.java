57768055a7a38f5a4dc883aaa963aa30a6d6ea74
Plamen Tanov <1.plamen@gmail.com>
2010-07-26 19:31:16 +0300

    Coordinates are fetched from page

--- src/main/java/eu/tanov/sumc/crawler/coordinates/CoordinatesProvider.java
+++ src/main/java/eu/tanov/sumc/crawler/coordinates/CoordinatesProvider.java
@@ -1,15 +1,25 @@
 package eu.tanov.sumc.crawler.coordinates;
 
+import java.util.List;
+
 import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import eu.tanov.sumc.crawler.model.BusStop;
 import eu.tanov.sumc.crawler.util.BgmapsHelper;
+import eu.tanov.sumc.crawler.util.WaitHelper;
 import eu.tanov.sumc.crawler.util.WebElementHelper;
+import eu.tanov.sumc.crawler.util.WaitHelper.Condition;
 
 public class CoordinatesProvider {
+	private static final String ID_FLAG_LOCATION_SELECTED = "FLAG_LOCATION_SELECTED";
+	private static final String ID_COORDINATES = "latlon";
+	
+
 	private static final Logger log = Logger.getLogger(CoordinatesProvider.class.getName());
 
 	private static final String URL_LOCATION = "http://www.getlatlon.com/?Sofia";
@@ -24,11 +34,29 @@ public class CoordinatesProvider {
 		prepareLocation(busStop);
 		prepareBgmaps(busStop);
 		
-		//TODO wait for element "FLAG_LOCATION_SELECTED" to be created
-		//TODO get content of elmement "latlon"
-		//TODO parse it (split by ,)
+		log.info("Place cross at the bus stop and click on button \"This is bus stop\"");
+		WaitHelper.waitForCondition(new Condition() {
+			
+			public boolean completed() {
+				final List<WebElement> matched = location.findElements(By.id(ID_FLAG_LOCATION_SELECTED));
+				
+				return !matched.isEmpty();
+			}
+		}, -1);
 		
-		//TODO set to busStop
+		final WebElement coordinatesHolder = location.findElement(By.id(ID_COORDINATES));
+		final String coordinates = WebElementHelper.getText(coordinatesHolder);
+
+		setCoordinates(busStop, coordinates);
+	}
+
+	private void setCoordinates(BusStop busStop, String coordinates) {
+		final String[] split = coordinates.split(", ");
+		if (split.length!=2) {
+			throw new IllegalStateException("Could not parse coordinates: "+coordinates);
+		}
+		busStop.setLat(Double.valueOf(split[0]));
+		busStop.setLon(Double.valueOf(split[1]));
 	}
 
 	private void prepareBgmaps(BusStop busStop) {
@@ -47,13 +75,15 @@ public class CoordinatesProvider {
 		final JavascriptExecutor javascriptExecutor = WebElementHelper.toJavascriptExecutor(location);
 		javascriptExecutor.executeScript("var button = document.createElement('input');" +
 				"button.type='button';" +
-				"button.value = 'This is bus station \"'+arguments[0]+'\"';" +
+				"button.setAttribute('id', 'BUTTON_LOCATION_SELECTED');" +
+				"button.value = 'This is bus stop \"'+arguments[0]+'\"';" +
 				"button.setAttribute('onClick'," +
 					"'var flag = document.createElement(\"div\");" +
-					"flag.id=\"FLAG_LOCATION_SELECTED\";" +
-					"document.getElementById(\"latlon\").parentElement.appendChild(flag);" +
+					"flag.id=\""+ID_FLAG_LOCATION_SELECTED+"\";" +
+					"document.getElementById(\""+ID_COORDINATES+"\").parentElement.appendChild(flag);" +
+					"document.getElementById(\"BUTTON_LOCATION_SELECTED\").value=\"Please wait...\";" +
 					"');" +
-				"document.getElementById('latlon').parentElement.appendChild(button);"
+				"document.getElementById('"+ID_COORDINATES+"').parentElement.appendChild(button);"
 					,
 				busStop.getLabel()
 		);
@@ -65,7 +95,7 @@ public class CoordinatesProvider {
 
 		javascriptExecutor.executeScript("var img = document.createElement('img');" +
 				"img.src = arguments[0];" +
-				"document.getElementById('latlon').parentElement.appendChild(img);",
+				"document.getElementById('"+ID_COORDINATES+"').parentElement.appendChild(img);",
 				BgmapsHelper.getBgmapsImage(busStop.getCode())
 		);
 		
